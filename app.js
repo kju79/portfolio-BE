316e9c0985a89d17260ea9a64883200a317f66c1
kju79 <rwgalaxy3@gmail.com>
2020-08-17 14:12:54 +0200

    heroku changes

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ var sendmailRouter = require("./routes/sendmail");
 
 var app = express();
 
+let port = 5000;
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
@@ -29,15 +31,8 @@ app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
+app.listen(process.env.PORT || 5000, () =>
+  console.log(`Backend of PORTFOLIO is running`)
+);
 
 module.exports = app;
