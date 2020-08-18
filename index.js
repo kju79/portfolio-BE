3cadd37caf524efd83dc98258b8bc6d1163bbe99
kju79 <rwgalaxy3@gmail.com>
2020-08-18 11:50:21 +0200

    stuff

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
-const dotenv = require("dotenv");
 const cors = require("cors");
+const path = require("path");
 
-dotenv.config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
-porto = process.env.PORT || 5000;
+const porto = process.env.PORT || 5000;
 
-var createError = require("http-errors");
+// var createError = require("http-errors");
 
 var indexRouter = require("./routes/index");
 var sendmailRouter = require("./routes/sendmail");
@@ -16,13 +18,16 @@ app.use(cors());
 
 app.use(express.json());
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+
 app.use("/", indexRouter);
 app.use("/sendmail", sendmailRouter);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+// // catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
 
 app.listen(porto, () =>
   console.log(`Backend of PORTFOLIO is running on ${porto}`)
